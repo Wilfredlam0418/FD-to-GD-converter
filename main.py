6bea62b762fa4714a3a66c3bcd386355ba24f30d
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2026-06-27 10:44:49 +0800

    Small saws

--- main.py
+++ main.py
@@ -318,7 +318,10 @@ for i in range(len(level_data)):
 			for k in fd_values.object_list[level_data[i][j]]:
 				if not ("tags" in k or "saw" in k):
 					if place_saw:
-						object_used = copy.deepcopy(fd_values.object_list[126][0])
+						if level[i][j] in saw_small:
+							object_used = copy.deepcopy(fd_values.saw_small[level[i][j]][0])
+						else:
+							object_used = copy.deepcopy(fd_values.object_list[126][0])
 					else:
 						object_used = copy.deepcopy(k)
 					place_object(object_used, True, not ("don't modify" in object_used and object_used["don't modify"]))
